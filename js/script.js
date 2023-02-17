5ffffe2d00101ea3f2df298f75055219110643df
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2023-02-16 23:46:12 -0300

    javascript

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// Separar menu links
+
+const linksMenu = document.querySelectorAll(".header-menu li a");
+function Clique(e) {
+  const href = e.href;
+  const url = document.location.href;
+  if (href.includes(url)) {
+    e.classList.add("ativo");
+  }
+}
+linksMenu.forEach(Clique);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
